506b000b0e478488738949f2256dbf1a7408891d
wdarochasilva <00001133140014SP@al.educacao.sp.gov.br>
2024-11-27 15:42:33 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Química',
+    'O que é fussão nuclear?',
+    'A fusão nuclear é uma união de núcleos gerando núcleos mais pesados'
+)
+
+criaCartao(
+    'Física',
+    'O que é energia nuclear?',
+    'Energia nuclear é a energia produzida nas usinas termonucleares, que utilizam o urânio e outros elementos, como combustível'
+)
+
+criaCartao(
+    'Química',
+    'Qual é o elemento mais utilizado na fissão nuclear?',
+    'Urânio'
+)
+
+criaCartao(
+    'Química',
+    'O que é química orgânica?',
+    'A Química Orgânica é a subárea da Química que explora as propriedades, os aspectos estruturais e a reatividade dos compostos orgânicos.'
+)
